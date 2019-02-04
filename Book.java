067fb9e485e852bac97bdea11969b1f7d0b83a64
Ernesto <neneern@yahoo.com>
2019-02-04 11:34:43 -0500

    added name and data
    name and date

--- Book.java
+++ Book.java
@@ -3,8 +3,8 @@
  * This might form part of a larger application such
  * as a library system, for instance.
  *
- * @author (Insert your name here.)
- * @version (Insert today's date here.)
+ * @author (Ernesto Cuellar)
+ * @version (2.4.2019)
  */
 class Book
 {
